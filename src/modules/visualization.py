e7264082ce984e83d435f9a76a50bda2451a36e5
HansEscobedo <hans.escobedo@alumnos.ucn.cl>
2024-06-26 18:24:10 -0400

    feat:block_value

--- src/modules/visualization.py
+++ src/modules/visualization.py
@@ -6,14 +6,35 @@ import matplotlib.pyplot as plt
 import io
 import base64
 
-
+metal_price = 1800000  # Ejemplo de valor
+metal_recovery = 0.85 # Ejemplo de valor
+mining_cost = 2.5  # Ejemplo de valor
+processing_cost = 5  # Ejemplo de valor
 def load_scenario(file_path):
     columns = ['X', 'Y', 'Z', 'Tonelaje total del bloque', 'metal 1', 'metal 2']
     data = pd.read_csv(file_path, header=None, names=columns)
     data['Z'] = -data['Z']  # Hacer que el valor de Z sea negativo al leer los datos
     data['Ley'] = data['metal 1'] / data['Tonelaje total del bloque']  # Calcular la ley del mineral
+
+
+    #Aquí se llama la función para hacer el cálculo del valor del bloque
+    data['Valor'] = data.apply(lambda row: calculate_block_value(
+        row['Ley'], row['Tonelaje total del bloque']), axis=1)
+
     return data
 
+
+def calculate_block_value(ley, tonelaje):
+    # Primera fórmula
+    formula_1 = ley * metal_price * metal_recovery - (mining_cost + processing_cost) * tonelaje
+    # Segunda fórmula
+    formula_2 = -(mining_cost * tonelaje)
+
+    # Comparar y devolver el valor correspondiente
+    if formula_2 > formula_1:
+        return formula_2
+    else:
+        return formula_1
 
 def create_graph(data):
     G = nx.DiGraph()
@@ -54,12 +75,14 @@ def visualize_scenario(data, mine_plan, period_limit):
     metal_1 = data['metal 1'].astype(float)
     metal_2 = data['metal 2'].astype(float)
     ley = data['Ley'].astype(float)
+    valor = data['Valor'].astype(float)
 
     points = pv.PolyData(np.column_stack((x, y, z)).astype(np.float32))
     points['Tonelaje'] = tonelaje
     points['Metal 1'] = metal_1
     points['Metal 2'] = metal_2
     points['Ley'] = ley
+    points['Valor'] = valor
     points['X'] = x
     points['Y'] = y
     points['Z'] = z
@@ -113,7 +136,7 @@ def visualize_2d(data, axis, axis_value):
 
     # Crear la visualización en 2D
     fig, ax = plt.subplots()
-    scatter = ax.scatter(x_vals, y_vals, c=filtered_data['Ley'], cmap='viridis')
+    scatter = ax.scatter(x_vals, y_vals, c=filtered_data['Ley'], cmap='cividis')
     ax.set_xlabel('Y' if axis == 'X' else 'X')
     ax.set_ylabel('Z' if axis in ['X', 'Y'] else 'Y')
     fig.colorbar(scatter, ax=ax, label='Ley')
